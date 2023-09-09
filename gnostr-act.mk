d7d1c5701dbdf92be920c052bb68149e2f118d74
randymcmillan <randy.lee.mcmillan@gmail.com>
2023-09-09 14:11:31 -0400

    make:install-gnostr-org

---
+++ gnostr-act.mk
@@ -0,0 +1,9 @@
+gnostr-act:##
+	@touch ~/GITHUB_TOKEN.txt
+	@export $(cat ~/GITHUB_TOKEN.txt) && act -C $(PWD) -vbr -W $(PWD)/.github/workflows/$@.yml
+gnostr-static:##
+	@touch ~/GITHUB_TOKEN.txt
+	@export $(cat ~/GITHUB_TOKEN.txt) && act -C $(PWD) -vbr -W $(PWD)/.github/workflows/$@.yml
+
+# vim: set noexpandtab:
+# vim: set setfiletype make
