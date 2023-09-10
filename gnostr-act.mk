10504e2d397470ad5d6f065114daf177a0c512f9
randymcmillan <randy.lee.mcmillan@gmail.com>
2023-09-09 23:04:51 -0400

    gnostr-act.mk:gnostr-git-docker-server

--- gnostr-act.mk
+++ gnostr-act.mk
@@ -4,15 +4,18 @@ gnostr-act:##
 gnostr-static:##
 	@touch ~/GITHUB_TOKEN.txt
 	@export $(cat ~/GITHUB_TOKEN.txt) && act -C $(PWD) -vbr -W $(PWD)/.github/workflows/$@.yml
+.PHONY:gnostr-docker-git-server
 gnostr-docker-git-server:docker-start
 	@docker build . --tag gnostr.org:latest --no-cache
 	@docker run --detach \
-  --name gnostr.org \
-  --volume $(HOME):/home/$(shell whoami) \
-  --volume $(PWD):/srv/git \
-  --env GIT_PASSWORD=$(APP_KEY) \
-  --publish 2222:22 \
-  gnostr.org:latest
+    --name gnostr.org.$(WEEBLE).$(WOBBLE).$(BLOCKHEIGHT) \
+    --volume $(HOME):/home/$(shell whoami) \
+    --volume $(PWD):/srv/git \
+    --env GIT_PASSWORD=$(APP_KEY) \
+    --publish 2222:22 \
+    --publish 6012:6102 \
+    --publish 443:443 \
+    gnostr.org:latest
 
 # vim: set noexpandtab:
 # vim: set setfiletype make
