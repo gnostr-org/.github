00000ff84d5f4533aae08e33ffb0f5f9acad07a1
randymcmillan <randy.lee.mcmillan@gmail.com>
2023-09-15 17:42:32 -0400

    2097/691886/807879/00/00047be5/gnostr.org

--- patch.demo.sh
+++ patch.demo.sh
@@ -1,75 +1,89 @@
-# `tre` is a shorthand for `tree` with hidden files and color enabled, ignoring
-# the `.git` directory, listing directories first. The output gets piped into
-# `less` with options to preserve color and line numbers, unless the output is
-# small enough for one screen.
-#
-# examples:
-# ./patch.demo.sh | jq .[0].contents | jq .[0].contents | jq .[].name
-# ./patch.demo.sh | jq -c
-#
-function tre() {
-	## tree -aC -I '' --dirsfirst "$@" | less -FRNX;
-	## tree -aC -I '' --dirsfirst "$@";
-	## tree -faCR -I '' --dirsfirst .gnostr | tr -d "|--" | tr -d "\`"
-    FILES=$(tree --noreport -faCRJ -I '.DS_Store' --dirsfirst .gnostr | tr -d "|--" | tr -d "\`")
-    ## echo $FILES
-    for f in $FILES; do
-        [ -L "${f%/}" ] && continue
-            echo $f;
-    done
+#!/usr/bin/env bash
+
+declare REPO
+REPO=$(pwd | grep -o '[^/]*$')
+declare BRANCH
+BRANCH=$(git branch --show-current)
+declare UTC_SECS
+UTC_SECS=$(date +%s)
+
+declare REPO_PATH
+REPO_PATH=$PWD/$BRANCH/$UTC_SECS
+echo $REPO_PATH;
+
+declare FILES
+declare PATCHES
+
+## examples:
+## ./patch.demo.sh | jq .[0].contents | jq .[0].contents | jq .[].name
+## ./patch.demo.sh | jq -c
+##
+## tree -aC -I '' --dirsfirst "$@" | less -FRNX;
+## tree -aC -I '' --dirsfirst "$@";
+## tree -faCR -I '' --dirsfirst .gnostr | tr -d "|--" | tr -d "\`"
+
+function gnostr-tree(){
+
+FILES=$(tree --noreport -faCRJ -I '.DS_Store' --dirsfirst .gnostr | tr -d "|--" | tr -d "\`")
+## echo $FILES
+for f in $FILES; do
+    [ -L "${f%/}" ] && continue
+        echo $f;
+done
+
 }
-function gnostr-list-patches(){
+
+function gnostr-patches(){
 
 PATCHES=$(ls .gnostr/patches/*.json)
 
 }
+
 ## for d in .*/ ; do
 ##     [ -L "${d%/}" ] && continue
 ##     echo "$d"
 ## done
+
 for td in .gnostr/ ; do
+
     [ -L "${td%/}" ] && continue
-    ## echo "td=$td"
+
     for d in $td ; do
+
         [ -L "${d%/}" ] && continue
-        ## echo "d=$d"
-        ## tre $d
-        gnostr-list-patches
+
+        gnostr-patches
+
         for f in $PATCHES; do
-            cat $f | jq -rM .content
-            #exit
+
+            touch $REPO_PATH
+            cat $f | jq -rM .content > /tmp/gnostr-patch.log
             git apply --allow-empty  <(cat $f)
-            gnostr-legit . -p 00000 -m "$(date +%s)/$(pwd | grep -o '[^/]*$')/$(git branch --show-current)" | gnostr-cat -u wss://relay.damus.io | $(grep 'OK') || echo "working..."
+
+            gnostr-legit . \
+                -p 00000 \
+                ## -m "$(date +%s)/$(pwd | grep -o '[^/]*$')/$(git branch --show-current)" | \
+                -m "$REPO_PATH" | \
+                gnostr-cat -u wss://relay.damus.io | \
+                $(grep 'OK') || echo "working..."
 
         done
     done
 
 done
-## for d in .gnostr/ ; do
-##     [ -L "${d%/}" ] && continue
-##     echo "$d"
-##         for file in d
-##         do
-##             echo $file
-##             for ((i = 0; i < 3; i++))
-##             do
-##                 name=${file##*/}
-##                 echo ${file##*/}
-##                 base=${name%.txt}
-##                 echo ${name%.txt}
-##             done
-##         done
-## done
-exit
-gnostr-git apply \
-    --ignore-whitespace \
-    --ignore-space-change \
-    --allow-empty \
-    --allow-overlap -Nv \
-    --inaccurate-eof \
-    --reverse \
-    --recount \
-    --cached \
-    --exclude README.md \
-    --exclude .gnostr/config.json \
-    <(cat .gnostr/patches/* | jq -rM .content)
+
+## function gnostr-git-apply(){
+##
+## gnostr-git apply \
+##     --ignore-whitespace \
+##     --ignore-space-change \
+##     --allow-empty \
+##     --allow-overlap -Nv \
+##     --inaccurate-eof \
+##     --reverse \
+##     --recount \
+##     --cached \
+##     --exclude README.md \
+##     --exclude .gnostr/config.json \
+##     <(cat .gnostr/patches/* | jq -rM .content)
+## }
