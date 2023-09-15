00000721e65322e109063a37ba495557bc5423da
randymcmillan <randy.lee.mcmillan@gmail.com>
2023-09-15 16:41:39 -0400

    2097/711299/807868/03/000b72c6/date +%s

--- patch.demo.sh
+++ patch.demo.sh
@@ -38,7 +38,7 @@ for td in .gnostr/ ; do
         for f in $PATCHES; do
 
             git apply --allow-empty  <(cat $f)
-            gnostr-legit . -p 00000 -m "$(echo date +%s)"
+            gnostr-legit . -p 00000 -m "$(echo date +%s)" | gnostr-relays
 
         done
     done
